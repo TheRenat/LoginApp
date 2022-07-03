e0cfcea36dca6ce259fa2710c21355bc40c4691c
TheRenat <therenat@mail.ru>
2022-07-03 22:15:20 +0300

    add logic

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,32 +9,25 @@ import UIKit
 
 @IBDesignable
 class WelcomeViewController: UIViewController {
-    
-    
-//    @IBInspectable var topColor: UIColor = .red
-//    @IBInspectable var buttomColor: UIColor = .blue
-   
+
     @IBOutlet var backgroundView: UIView!
     
     @IBOutlet var welcomeLabel: UILabel!
     
     var userName: String!
+    @IBInspectable var firstColor: UIColor = .clear
+    @IBInspectable var secondColor: UIColor = .clear
     
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeLabel.text = "Welcome, \(userName ?? "")"
         
-        
+//        let newLayer = CAGradientLayer()
+//        newLayer.colors = [UIColor.blue.cgColor, UIColor.green.cgColor]
+//        newLayer.frame = view.bounds
+//        
+//        view.layer.insertSublayer(newLayer, at: 0)
     }
-    
-//    override func viewDidLayoutSubviews() {
-//        let gradientLayer = CAGradientLayer()
-//        gradientLayer.frame = view.bounds
-//        gradientLayer.colors = [topColor.cgColor, buttomColor.cgColor]
-//        backgroundView.layer.addSublayer(gradientLayer)
-//    }
-    
-    
     
     @IBAction func logoutButtonPressed() {
         dismiss(animated: true)
