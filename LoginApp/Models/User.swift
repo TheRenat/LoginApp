d42bd890dde4f1123202e504126b4420bf14c69c
TheRenat <therenat@mail.ru>
2022-07-07 23:04:55 +0300

    correcting the project

--- LoginApp/Models/User.swift
+++ LoginApp/Models/User.swift
@@ -14,3 +14,42 @@ struct User {
         User(login: "Renat", password: "Password", person: Person.getPerson())
     }
 }
+
+struct Person {
+    let name: String
+    let infoAboutPerson: String
+    let image: String
+    let storyOfLife: String
+    
+    static func getPerson() -> Person {
+        Person(
+            name: "Renat Ibragimov",
+            infoAboutPerson:
+            """
+            Имя: Ренат
+            Фамилия: Ибрагимов
+            Возраст: 30 лет
+            
+            Образование: Высшее
+            
+            Текущее место работы: АО "Агентство ВТН"
+            Должность: Key Account Manager
+            
+            Предыдущие места работы:
+            - ЗАО "Контроль качества"
+            - АО "KNAUF Rus"
+            """,
+            image: "",
+            storyOfLife:
+                """
+                Родился в небольшом городе Ленинградской области в 1992 году.
+                В возрасте семи лет родители решили отдать меня на плавание, где
+                и прошли все мое детство и юность. В 19 лет сходил в армию, после
+                чего поступил в ИТМО на факультет "Технологического менеджмента",
+                в последствии окончил его. На данный момент работаю в сфере логистики.
+                
+                Хобби - велоспорт, компьютерные игры.
+                """
+        )
+    }
+}
